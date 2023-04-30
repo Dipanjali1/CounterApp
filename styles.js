3d5dbe5ee5448376b7cf4959163f9ebf872abf97
Dipanjali Gubreley <78899804+Dipanjali1@users.noreply.github.com>
2023-04-30 16:05:18 +0530

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,37 @@
+.App {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  text-align: center;
+}
+#main {
+  background: linear-gradient(130deg, white, grey, white);
+}
+button {
+  border-radius: 5px;
+  border: 5px solid grey;
+  width: 200px;
+  height: 60px;
+  margin: 10px 10px 10px 18px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  background: linear-gradient(130deg, white, grey);
+  cursor: pointer;
+  outline: none;
+  transition: 0.2s all;
+}
+button:active {
+  transform: scale(0.98);
+  box-shadow: 3px 2px 22px 1px rgba(0, 0, 0, 0.24);
+}
+h1 {
+  margin-left: 50%;
+  font-size: 40px;
+}
+#fac {
+  font-size: 20px;
+  margin-left: 18px;
+  height: 110px;
+  width: 100%;
+  //border: 1px solid black;
+}
+span {
+  margin-left: 46%;
+}
